3f232c0966bac42d3b051c6b48f0f7ff200861b7
pvorona <vorona.pavel.nickolaevich@gmail.com>
2022-01-12 19:47:19 +0300

    add ts types to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import filesize from 'rollup-plugin-filesize'
-import typescript from '@rollup/plugin-typescript'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import typescript from 'rollup-plugin-typescript2'
+
+// const pkg = require('./package.json')
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: './dist/index.js',
-    format: 'umd',
-    name: 'observable',
-  },
-  plugins: [typescript(), filesize()],
+  output: [
+    { dir: 'dist' },
+    // { file: pkg.main, name: 'observable', format: 'umd', sourcemap: true },
+    // { file: pkg.module, format: 'es', sourcemap: true },
+  ],
+  plugins: [typescript({ declaration: true }), filesize(), sourceMaps()],
+  preserveModules: true,
 }
