5f140d26cc6c163326dc0e7c6a92266065bec370
pvorona <vorona.pavel.nickolaevich@gmail.com>
2022-01-23 22:14:41 +0300

    Add `lint-staged`

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,9 @@ import filesize from 'rollup-plugin-filesize'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 
-// const pkg = require('./package.json')
-
 export default {
   input: 'src/index.ts',
-  output: [
-    { dir: 'dist' },
-    // { file: pkg.main, name: 'observable', format: 'umd', sourcemap: true },
-    // { file: pkg.module, format: 'es', sourcemap: true },
-  ],
+  output: [{ dir: 'dist' }],
   plugins: [typescript({ declaration: true }), filesize(), sourceMaps()],
   preserveModules: true,
 }
